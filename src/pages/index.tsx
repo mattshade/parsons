6f20e91c629a88a96290d2a09b5ac69708134a68
matt-shade-nbcuni <matt.shade@nbcuni.com>
2022-06-21 23:37:39 -0400

    updates index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi Jeff, we're Parson's Cabinetry<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>Running on Next.js and Netlify.</h2>
           <SocialList />
         </div>
       </div>
